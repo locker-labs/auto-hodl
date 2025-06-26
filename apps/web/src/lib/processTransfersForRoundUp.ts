8b06a0ef2cd55adfc8af9830c2a2c888b48a6ffe
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-06-26 19:32:28 +0530

    chore: add redeem delegation txn

--- apps/web/src/lib/processTransfersForRoundUp.ts
+++ apps/web/src/lib/processTransfersForRoundUp.ts
@@ -1,15 +1,13 @@
-import { createExecution,ExecutionStruct,DeleGatorEnvironment} from "@metamask/delegation-toolkit";
+import { createExecution,DelegationFramework, Delegation, SINGLE_DEFAULT_MODE, getDeleGatorEnvironment} from "@metamask/delegation-toolkit";
 import { encodeSupplyCallData, erc20Abi, encodeApproveTokensCallData } from './yield /aave';
 import { delegateWalletClient } from '@/clients/delegateWalletClient';
-import { aavePoolAbi } from './yield /aavePoolAbi';
 import { publicClient } from '@/clients/publicClient';
 import { AAVE_POOL_ADDRESS } from '@/config';
 import { ITransfer } from './types';
+import { VIEM_CHAIN } from '@/config';
 
 
-
-
-export function processTransferForRoundUp(transfer: ITransfer) {
+export async function processTransferForRoundUp(transfer: ITransfer) {
   console.log('Processing transfer:', {
     hash: transfer.transactionHash,
     from: transfer.from,
@@ -29,27 +27,27 @@ export function processTransferForRoundUp(transfer: ITransfer) {
   // NOTE: You must replace these with actual values from your DB or logic
   const roundUpAmount = 100;
   const tokenBalance = 100_000;
+  const delegations: Delegation[] = [];
   const asset = transfer.tokenAddress as `0x${string}`;
   const amount = calculateSavingsAmount(BigInt(transfer.amount), BigInt(roundUpAmount), BigInt(tokenBalance));
   const onBehalfOf = transfer.from as `0x${string}`; // Or fetched savings address
   const encodedSupplyCallData = encodeSupplyCallData(asset, amount, onBehalfOf);
   const encodedApproveCallData = encodeApproveTokensCallData(asset, BigInt(1000000000000000000));
-  const approvalExecution = createExecution({
-    target: asset,
-    callData: encodedApproveCallData,
-    value: 0n,
-  });
-  const supplyExecution = createExecution({
-    target: AAVE_POOL_ADDRESS,
-    callData: encodedSupplyCallData,
-    value: 0n,
-  });
+  const approvalExecution = createExecution(asset,0n,encodedApproveCallData,);
+  const supplyExecution = createExecution(AAVE_POOL_ADDRESS,0n,encodedSupplyCallData,);
   const executions = [approvalExecution, supplyExecution];
-  // const redeemDelegationCalldata = DelegationFramework.encode.redeemDelegations({
-  //   delegations: [ delegations ],
-  //   modes: [ SINGLE_DEFAULT_MODE ],
-  //   executions: [ executions ]
-  // });
+  const redeemDelegationCalldata = DelegationFramework.encode.redeemDelegations({
+    delegations: [ delegations ],
+    modes: [ SINGLE_DEFAULT_MODE ],
+    executions: [ executions ]
+  });
+  const transactionHash = await delegateWalletClient.sendTransaction({
+    to: getDeleGatorEnvironment(VIEM_CHAIN.id).DelegationManager,
+    data: redeemDelegationCalldata,
+    VIEM_CHAIN,
+  });
+  console.log("Redeem Delegation Transaction Hash:", transactionHash);
+  return transactionHash;
 }
 
 
