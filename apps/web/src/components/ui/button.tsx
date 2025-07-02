a5ae08700c3a7bf29fecdfcb0e12ef03ac301d02
thisisashugupta <ashugpt1812@gmail.com>
2025-07-02 05:38:26 +0530

    fix(ui): button style fix

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -42,17 +42,9 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
   }) {
-  const customClassName =
-    'h-12 rounded-xl font-bold text-base cursor-pointer bg-[#ff7a45] hover:bg-[#ff6a35] disabled:bg-[#ffb399] text-white';
   const Comp = asChild ? Slot : 'button';
 
-  return (
-    <Comp
-      data-slot='button'
-      className={cn(buttonVariants({ variant, size, className: `${customClassName} ${className}` }))}
-      {...props}
-    />
-  );
+  return <Comp data-slot='button' className={cn(buttonVariants({ variant, size, className }))} {...props} />;
 }
 
 export { Button, buttonVariants };
