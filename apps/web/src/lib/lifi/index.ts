539842c9b3b1770d89e1bb9dbc1df4027033496f
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-07-02 19:44:41 +0530

    chore: add route execution

--- apps/web/src/lib/lifi/index.ts
+++ apps/web/src/lib/lifi/index.ts
@@ -1,8 +1,17 @@
-import { getRoutes,getQuote,RouteExtended, RoutesRequest, AllowDenyPrefer, QuoteRequest, getStepTransaction } from "@lifi/sdk";
+import {
+  getRoutes,
+  getQuote,
+  RouteExtended,
+  RoutesRequest,
+  QuoteRequest,
+  getStepTransaction,
+} from "@lifi/sdk";
 import { VIEM_CHAIN } from "@/config";
 import { TokenAddressMap } from "../constants";
 import { Delegation, ExecutionStruct } from "@metamask/delegation-toolkit";
 import { redeemAaveDelegations } from "../processTransfersForRoundUp";
+import { encodeApproveTokensCallData } from "../yield/aave";
+import { getStatus } from "@lifi/sdk/";
 
 /**
  * Fetches routes for a given chain and amount using the LiFi SDK.
@@ -11,63 +20,107 @@ import { redeemAaveDelegations } from "../processTransfersForRoundUp";
  * @param {string} amount - The amount to be transferred.
  * @returns {Promise<Routes[]>} - A promise that resolves to an array of routes.
  */
-async function requestRoute(delegatorAddress: `0x${string}`, chainId: number, amount: string) {
-    const routesRequest: RoutesRequest = {
-        fromChainId: VIEM_CHAIN.id,
-        toChainId: chainId,
-        fromTokenAddress: TokenAddressMap[VIEM_CHAIN.id],
-        toTokenAddress: TokenAddressMap[chainId],
-        fromAmount: amount,
-        fromAddress: delegatorAddress,
-        options: {
-            bridges: {
-                allow: ["mayanMCTP"],
-            }
-        }
-    }
-    const result = await getRoutes(routesRequest);
-    return result.routes;
+async function requestRoute(
+  delegatorAddress: `0x${string}`,
+  chainId: number,
+  amount: string
+) {
+  const routesRequest: RoutesRequest = {
+    fromChainId: VIEM_CHAIN.id,
+    toChainId: chainId,
+    fromTokenAddress: TokenAddressMap[VIEM_CHAIN.id],
+    toTokenAddress: TokenAddressMap[chainId],
+    fromAmount: amount,
+    fromAddress: delegatorAddress,
+    options: {
+      bridges: {
+        allow: ["mayanMCTP"],
+      },
+    },
+  };
+  const result = await getRoutes(routesRequest);
+  return result.routes;
 }
 
-async function requestQuote(chainId: number, amount: string, senderAddress: `0x${string}`) {
-    const quoteRequest: QuoteRequest = {
-        fromChain: VIEM_CHAIN.id,
-        toChain: chainId,
-        fromToken: TokenAddressMap[VIEM_CHAIN.id],
-        toToken: TokenAddressMap[chainId],
-        fromAmount: amount,
-        fromAddress: senderAddress,
-        allowBridges: ["celercircle"], // Only allow CCTP V2
+export async function requestQuote(
+  chainId: number,
+  amount: string,
+  senderAddress: `0x${string}`
+) {
+  const quoteRequest: QuoteRequest = {
+    fromChain: VIEM_CHAIN.id,
+    toChain: chainId,
+    fromToken: TokenAddressMap[VIEM_CHAIN.id],
+    toToken: TokenAddressMap[chainId],
+    fromAmount: amount,
+    fromAddress: senderAddress,
+    allowBridges: ["celercircle"], // Only allow CCTP V2
+  };
+  const quote = await getQuote(quoteRequest);
+  return quote;
+}
+
+export const getTransactionLinks = (route: RouteExtended) => {
+  route.steps.forEach((step, index) => {
+    step.execution?.process.forEach((process) => {
+      if (process.txHash) {
+        console.log(
+          `Transaction Hash for Step ${index + 1}, Process ${process.type}:`,
+          process.txHash
+        );
+      }
+    });
+  });
+};
+
+export async function bridgeAndRedeem(
+  delegation: Delegation,
+  chainId: number,
+  amount: string,
+  senderAddress: `0x${string}`
+) {
+  const routes = await requestRoute(senderAddress, chainId, amount);
+
+  for (const step of routes[0].steps) {
+    const data = await getStepTransaction(step);
+    const approvalCallData = encodeApproveTokensCallData(
+      data.transactionRequest?.to as `0x${string}`,
+      BigInt(amount)
+    );
+    const approvalExecution: ExecutionStruct = {
+      target: TokenAddressMap[VIEM_CHAIN.id],
+      value: BigInt(0),
+      callData: approvalCallData,
     };
-    const quote = await getQuote(quoteRequest);
-    return quote;
+    const bridgeExecution: ExecutionStruct = {
+      target: data.transactionRequest?.to as `0x${string}`,
+      value: BigInt(0),
+      callData: data.transactionRequest?.data as `0x${string}`,
+    };
 
-}
+    const transactionHash = await redeemAaveDelegations(
+      [delegation, delegation],
+      [approvalExecution, bridgeExecution]
+    );
+    let status;
+    do {
+      const result = await getStatus({
+        txHash: transactionHash,
+        fromChain: step.action.fromChainId,
+        toChain: step.action.toChainId,
+        bridge: step.tool,
+      });
+      status = result.status;
 
-const getTransactionLinks = (route: RouteExtended) => {
-    route.steps.forEach((step, index) => {
-      step.execution?.process.forEach((process) => {
-        if (process.txHash) {
-          console.log(
-            `Transaction Hash for Step ${index + 1}, Process ${process.type}:`,
-            process.txHash
-          )
-        }
-      })
-    })
-  }
+      console.log(`Transaction status for ${transactionHash}:`, status);
 
-export async function bridgeAndRedeem(delegation: Delegation,chainId: number, amount: string, senderAddress: `0x${string}`) {
-    const routes = await requestRoute(senderAddress,chainId, amount);
-    console.log("Routes:", routes);
+      // Wait for a short period before checking the status again
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    } while (status !== "DONE" && status !== "FAILED");
 
-    for (const step of routes[0].steps) {
-        const data = await getStepTransaction(step);
-        const execution: ExecutionStruct = {
-            target: data.transactionRequest?.to as `0x${string}`,
-            value: BigInt(0),
-            callData: data.transactionRequest?.data as `0x${string}`,
-        }
-        await redeemAaveDelegations([delegation], [execution]);
+    if (status === "FAILED") {
+      console.error(`Transaction ${transactionHash} failed`);
+      return;
     }
+  }
 }
