4f703a9a7d76086cd549e7e21ed36055fd2c5aa3
thisisashugupta <ashugpt1812@gmail.com>
2025-07-03 19:20:47 +0530

    feat: switch-chain-mode card color and copy changes

--- apps/web/src/components/ui/switch.tsx
+++ apps/web/src/components/ui/switch.tsx
@@ -18,7 +18,7 @@ function Switch({ className, ...props }: React.ComponentProps<typeof SwitchPrimi
       <SwitchPrimitive.Thumb
         data-slot='switch-thumb'
         className={cn(
-          'data-[state=unchecked]:bg-[#ff7a45]/70 bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0',
+          'data-[state=unchecked]:bg-[#ff7a45] bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0',
         )}
       />
     </SwitchPrimitive.Root>
