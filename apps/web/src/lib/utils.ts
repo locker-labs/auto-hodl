fc69f49fc441c7c1c3f10a0e3a2bf19a07059e5d
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-07-03 20:37:22 +0530

    feat: lifi caveats

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { getAaveCaveats } from './yield/caveats';
+import { MetaMaskSmartAccount } from '@metamask/delegation-toolkit';
+import { getLifiCaveats } from './lifi/caveats';
+import {EChainMode} from '@/enums/chainMode.enums';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getCaveats(delegatorAccount:MetaMaskSmartAccount,amount:bigint, mode: string) {
+  if (mode === EChainMode.SINGLE_CHAIN) {
+    return getAaveCaveats(delegatorAccount, amount);
+  } else if (mode === EChainMode.MULTI_CHAIN) {
+    return getLifiCaveats(delegatorAccount, amount);
+  }
+  return [];
 }
